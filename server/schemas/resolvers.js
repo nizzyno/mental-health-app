fa2a6df7d0ad0aa0f4ec883e17899b34b24975f9
peter <ptmartinezsound@gmail.com>
2021-07-15 09:03:30 -0500

    added JSON webtokens

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
 const { User } = require('../models/User');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -13,11 +15,27 @@ const resolvers = {
     },
 
     Mutation: {
-        addUser: async () => {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
+            return { token, user };
         },
-        login: async () => {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
 
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
         }
     }
 };
