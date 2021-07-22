248e5b861e367fa6af84895942633807fa29c531
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-07-22 16:10:02 -0500

    Add profile page state specific element rendering

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_ME, QUERY_USER } from '../utils/queries';
 import Auth from '../utils/auth';
 
 function Profile() {
+  const [isEditing, setIsEditing] = useState(false);
   const { username: userParam } = useParams();
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
@@ -34,6 +35,21 @@ function Profile() {
 
 
   const handleKeyDown = e => {
+    if(!isEditing)
+      setIsEditing(true)
+
+    const buttonsExist = doucment.querySelector('#formButtonContainer');
+    if(!buttonsExist){
+      let divEl = document.createElement('div');
+      divEl.id = "formButtonContainer";
+      divEl.innerHTML = `
+      <button type="submit" id="profileSubmitButton">Save</button>
+      <button type="button" id="profileCancelEdit">Cancel</button>
+      `
+      doucment.querySelector('#profileForm').append(divEl);
+    }
+
+
     const inputName = e.target.getAttribute("name");
     const errorBox = document.querySelector("#profileErrorBox");
     let errorMessage = "";
@@ -55,18 +71,33 @@ function Profile() {
         break;
 
       default: return;
-
-      if(errorMessage)
-        errorBox.innerText = errorMessage
     }
-  }
+
+    if(errorMessage)
+        errorBox.innerText = errorMessage
+  };
+
+
+  const handleCancel = e => {
+    document.querySelector('#profileUsername').value = user.username;
+    document.querySelector('#profileEmail').value = user.email;
+    document.querySelector('#profilePassword').value = user.password;
+    document.querySelector('#formButtonContainer').remove();
+    setIsEditing(false);
+  };
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setIsEditing(false);
+  };
 
   return (
   
   <div className="flex-column">
       <div id="profileErrorBox"></div>
       <h2>Profile:</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} id="profileForm">
         <div hidden="hidden" data-attribute={"" + user._id}></div>
         <label name="profileUsername">Username: </label>
         <input type="text" name="profileUsername" id="profileUsername" value={user.username} onKeyDown={handleKeyDown}></input>
@@ -74,7 +105,6 @@ function Profile() {
         <input type="text" name="profileEmail" id="profileEmail" value={user.email} onKeyDown={handleKeyDown}></input>
         <label name="profilePassword">Password: </label>
         <input type="text" name="profilePassword" id="profilePassword" value={user.password} onKeyDown={handleKeyDown}></input>
-        <button type="button" id="profileSubmitButton">Save</button>
       </form>
   </div>
   
