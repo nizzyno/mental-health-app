32c7a3ebfad2dc95ec408fcb0efa183545f6379d
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-07-22 15:32:04 -0500

    Add profile edit fields error handling

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -32,18 +32,48 @@ function Profile() {
     );
   }
 
+
+  const handleKeyDown = e => {
+    const inputName = e.target.getAttribute("name");
+    const errorBox = document.querySelector("#profileErrorBox");
+    let errorMessage = "";
+
+    switch(inputName){
+      case "profileUsername":
+        if(e.target.value.length === 0)
+          errorMessage = "Your username must be at least 1 character long."
+        break;
+      
+      case "profileEmail":
+        if(!e.target.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/))
+          errorMessage = "Your email must be in an email format"
+          break;
+
+      case "profilePassword":
+        if(!e.target.value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/))
+          errorMessage = "Your password must be between 8 and 15 characters long containing one upper and lowercase letters, number, and special character."
+        break;
+
+      default: return;
+
+      if(errorMessage)
+        errorBox.innerText = errorMessage
+    }
+  }
+
   return (
   
   <div className="flex-column">
+      <div id="profileErrorBox"></div>
       <h2>Profile:</h2>
       <form onSubmit={handleSubmit}>
         <div hidden="hidden" data-attribute={"" + user._id}></div>
         <label name="profileUsername">Username: </label>
-        <input type="text" name="profileUsername" id="profileUsername" value={user.username}></input>
+        <input type="text" name="profileUsername" id="profileUsername" value={user.username} onKeyDown={handleKeyDown}></input>
         <label name="profileEmail">Email: </label>
-        <input type="text" name="profileEmail" id="profileEmail" value={user.email}></input>
+        <input type="text" name="profileEmail" id="profileEmail" value={user.email} onKeyDown={handleKeyDown}></input>
         <label name="profilePassword">Password: </label>
-        <input type="text" name="profilePassword" id="profilePassword" value={user.password}></input>
+        <input type="text" name="profilePassword" id="profilePassword" value={user.password} onKeyDown={handleKeyDown}></input>
         <button type="button" id="profileSubmitButton">Save</button>
       </form>
   </div>
