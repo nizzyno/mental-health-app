434577aac82119435f16ee740f5369afc45cde81
peter <ptmartinezsound@gmail.com>
2021-07-25 15:43:49 -0500

    login and signin components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 // import Auth from '../../utils/auth';
+import Api from '../utils/api';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup'; 
+
 
 const Header = () => {
+
+
   return (
     <header className="">
       <div className="">
         <h1>HAVEN</h1>
 
         <nav className="text-center">
+        <Link to="/">Home</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Signup</Link>
+
         </nav>
       </div>
     </header>
