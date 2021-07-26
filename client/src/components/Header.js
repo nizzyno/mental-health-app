a3b37ad5c4c9289ee9a2c6e14b80b4ab0ecce817
peter <ptmartinezsound@gmail.com>
2021-07-26 01:59:22 -0500

    logout functionality

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,13 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import Auth from '../../utils/auth';
+import Auth from '../utils/auth.js';
 
 const Header = () => {
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
 
 
   return (
@@ -11,10 +16,20 @@ const Header = () => {
         <h1>HAVEN</h1>
 
         <nav className="text-center">
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          {Auth.loggedIn() ? (
+            <>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
 
+            <>
+              <Link to="/">Home</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
