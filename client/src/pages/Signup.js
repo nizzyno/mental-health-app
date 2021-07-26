bf87972557ad1a6af9b95b4c69dca5b4caafa8ab
Noah <mcbeasty11.nh@icloud.com>
2021-07-25 23:25:59 -0500

    installed dependencies

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/react-hooks';
 
 const Signup = () => {
   const [formState, setFormState] = useState({
