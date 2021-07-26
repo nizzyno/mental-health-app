10ed38abff9920bb106f63e4e27fcbb9794a78ed
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-26 14:29:20 -0500

    input placeholder for header image

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-import 'semantic-ui-css/semantic.min.css'
-import './assets/main.css'
+import React from 'react';
+
+import brain from '../../assets/cover/header/havenbrain.png'
+
+function NavBar(props) {
+const navBarLinks = [
+  {
+    navLink: 'Home',
+  },
+  {
+      navLink: 'Login',
+  },
+  {
+      navLink: 'Logout',
+  },
+  {
+      navLink: 'SignUp',
+  }]
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="bg-secondary mb-4 py-2 flex-row align-left">
+      <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <p> Haven Image Here </p>
+      </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
