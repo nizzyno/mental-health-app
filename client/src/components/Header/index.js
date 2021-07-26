ff69d416eafcd27eb901ab604604f4e319b9c7ca
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-26 15:00:52 -0500

    added main logo to header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import brain from '../../assets/header/havenbrain.png'
+import haven from '../../assets/header/haven.png'
 
-import brain from '../../assets/cover/header/havenbrain.png'
 
 function NavBar(props) {
 const navBarLinks = [
   {
     navLink: 'Home',
+    image: brain
   },
   {
       navLink: 'Login',
@@ -22,7 +24,7 @@ const Header = () => {
     <header>
       <nav className="bg-secondary mb-4 py-2 flex-row align-left">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        
+        <img src={haven} className='haven-header' alt='logo'></img>
       </div>
       </nav>
     </header>
