f350ee50520c3d7fa6c56c742eb376514afd8af3
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-26 14:21:11 -0500

    updated navbar / nav logo

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import brain from '../../assets/cover/header/havenbrain.png'
+
 function NavBar(props) {
 const navBarLinks = [
+  {
+    navLink: 'Home',
+  },
   {
       navLink: 'Login',
   },
@@ -17,7 +22,7 @@ const Header = () => {
     <header>
       <nav className="bg-secondary mb-4 py-2 flex-row align-left">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1>Haven</h1>
+        
       </div>
       </nav>
     </header>
