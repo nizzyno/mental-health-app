5051afb8cd897a750ed644104bf3c9c7796a3e9d
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-26 13:25:57 -0500

    updated navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+    <header>
+      <nav className="bg-secondary mb-4 py-2 flex-row align-left">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <h1>Haven</h1>
       </div>
+      </nav>
     </header>
   );
 };
